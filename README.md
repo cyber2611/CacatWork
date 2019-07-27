d5afbef9cf06d2deee81e9d0186a7d2c33be1236
cyber2611 <52934500+cyber2611@users.noreply.github.com>
2019-07-28 02:05:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Brother Id
+
+
+$ pkg update && pkg update
+$ pkg install python2
+$ pip2 install mechanize
+$ pip2 install requests
+$ pkg install git
+$ git clone https://github.com/cyber2611/Brother-Id
+$ ls
+$ cd Brother-Id
+$ ls
+$ sh luarbiasa.sh
---
+++ bangsa-indo.sh
@@ -0,0 +1,108 @@
+!#/bin/bash
+clear
+figlet  Brother ID | lolcat
+
+
+
+
+echo "
+[≠]=================================================================[≠]
+[≠]  * Author  : Brother                                          [≠]
+[≠]  * Support : Brother VictoryHacker[] / [!]JANGAN DIJUAL / BV [≠]
+[≠]  * GitHub  : https://github.com/cyber2611                     [≠]
+[≠]  *Decompiled by Yoga Wira                                       [≠]
+[≠]=================================================================[≠]"
+echo
+echo "TEKAN CTRL + Z JIKA MAU EXIT" | lolcat
+echo "{•}=========================================={•}" | lolcat
+echo "   {1} DARK_FACEBOOK V1.6....               " | lolcat
+echo "   {2} DARK_FACEBOOK V1.7....               " | lolcat
+echo "   {3} DARK_FACEBOOK PREMIUM....       | lolcat      "
+echo "   {4} DARK_FACEBOOK VVIP.....              " | lolcat
+echo "   {5} INSTALL BAHAN.....                   "
+echo "{•}=========================================={•}" | lolcat
+echo | lolcat
+read -p "Masukan Pilihan Anda Cok : " aku
+
+if [ $aku = 1 ] || [ $aku = 1 ]
+then
+clear
+echo "╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ v1.6" | lolcat
+sleep 3
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+pip2 install requests mechanize
+python2 SETAN.py
+fi
+
+if [ $aku = 2 ]  || [ $aku = 02 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ v1.7
+█████████      «----------✧----------»
+ ██ ██" | lolcat
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/TheMagizz/fb1.7
+cd fb1.7
+pip2 install requests mechanize
+python2 dark.py
+fi
+
+if [ $aku = 3 ] || [ $aku = 03 ]
+then
+clear
+echo "█▄█████▄█         ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+ █ ▼▼▼▼▼  - _ --_-- ╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+ █    _-_-- -_ --__  ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+ █ ▲▲▲▲▲ --  - _ -- ═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝  Premium
+ █████████         «==========✧==========»
+  ██ ██" | lolcat 
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+pip2 install -r requirements.txt
+python2 DarkFB.py
+fi
+
+if [ $aku = 4 ] || [ $aku = 04 ]
+then
+clear
+echo "█████████
+█▄█████▄█      ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+█▼▼▼▼▼ - _ --_--╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+█  _-_-- -_ --__ ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+█▲▲▲▲▲--  - _ --═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ VIP
+█████████      «°°°°°°°°°°✧°°°°°°°°°°»
+ ██ ██" | lolcat
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/TheMagizz/DarkVip
+cd DarkVip
+pip2 install -r requirements.txt
+python2 darkvip.py
+fi
+
+if [ $aku = 5 ] || [ $aku = 05 ]
+then
+clear
+$ pkg update && pkg update
+$ pkg install python2
+$ pip2 install mechanize
+$ pip2 install requests
+$ pkg install git
+$ git clone https://github.com/cyber2611/Brother-Id
+$ ls
+$ cd Brother-Id
+$ ls
+$ sh luarbiasa.sh
+fi
